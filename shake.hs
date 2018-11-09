a018d1b5e804dbe16ce997976d24c9cd5b343a26
OSANAI Kazuyoshi <osmium.k@gmail.com>
2018-11-09 19:21:34 +0900

    bump version v0.0.4

--- shake.hs
+++ shake.hs
@@ -29,7 +29,7 @@ import Data.Aeson.Lens
 import Control.Monad (forM_)
 import Network.HTTP.Types (urlDecode)
 
-version = "v0.0.3"
+version = "v0.0.4"
 description = ""
 
 tokenName = "GITHUB_RELEASE_TOKEN"
